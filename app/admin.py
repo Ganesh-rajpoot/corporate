91977af7207df09dee50e9896f74150a5260593f
Ganesh-rajpoot <ganeshrajpoot86@gmail.com>
2024-04-13 16:37:51 -0700

    Adding user model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'mobile', 'referral_code']
+    search_fields = ['username', 'email', 'mobile']
+    # Add more customization as needed
--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=15, primary_key=True)
+    password = models.CharField(max_length=128)
+    referral_code = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super(User, self).save(*args, **kwargs)
